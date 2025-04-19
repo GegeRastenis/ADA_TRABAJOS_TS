2b96d2f39d3be2b1eb6ea1f99174d296c305d1f3
Gegerastenis <giselle_rastenis@hotmail.com>
2025-04-19 17:06:18 -0300

    Subiendo Clase6 ejercicios1 - 3 al 6

--- Clase6/ActividadesAbstraccion/ejercicio3.ts
+++ Clase6/ActividadesAbstraccion/ejercicio3.ts
@@ -6,3 +6,29 @@ Crea una interfaz Usuario que tenga las propiedades:
 Implementa esta interfaz en una clase UsuarioConcreto. Luego, intenta 
 modificar la propiedad id para mostrar cómo se aplican las restricciones de solo 
 lectura. */
+
+interface Usuario{
+    nombre: string; 
+    edad?: number; 
+    readonly id: number
+}
+
+class UsuarioConcreto implements Usuario{
+
+    nombre: string; 
+    edad?: number; 
+    readonly id: number; 
+
+    constructor(nombre: string, edad: number, id: number){
+        this.nombre = nombre, 
+        this.edad = edad, 
+        this.id = id
+    }
+}
+
+const usuario = new UsuarioConcreto("Pedro", 15, 5); 
+console.log(`Nuevo usuario: `);
+console.log(usuario);
+
+
+
--- Clase6/ActividadesAbstraccion/ejercicio6.ts
+++ Clase6/ActividadesAbstraccion/ejercicio6.ts
@@ -8,3 +8,71 @@ Implementa dos subclases: Ropa y Electronica.
 • La subclase Electronica tendrá un 5% de descuento. 
 Añade un método concreto en la clase abstracta para mostrar la información del 
 producto.*/
+
+abstract class Producto{
+    
+    protected name: string; 
+    protected price: number
+
+    constructor(name: string, price: number) {
+
+        this.name = name; 
+        this.price = price
+        
+    }
+
+    abstract calcularDescuento(): void; 
+
+    describirProducto():void{
+
+        console.log(`Este producto ${this.name} cuesta: ${this.price}`);
+    }
+}
+
+class Ropa extends Producto{
+
+    constructor(){
+        super("Campera", 1500)
+    }
+
+    describirProducto(): void {
+        console.log(`Este producto ${this.name} cuesta: ${this.price}`);
+        
+    }
+
+    calcularDescuento(): void {
+        let descuento = this.price * 0.10; 
+        console.log(`Su descuento en esta prenda es de: ${descuento}`);
+        
+    }
+}
+
+class Electronica extends Producto{
+
+    constructor(){
+        super("Computadora Personal", 5000)
+    }
+
+    describirProducto(): void {
+        console.log(`Este producto ${this.name} cuesta: ${this.price}`);
+        
+    }
+
+    calcularDescuento(): void {
+        let descuento = this.price * 0.05; 
+        console.log(`Su descuento en este producto es de: ${descuento}`);
+        
+    }
+}
+
+
+
+
+
+const ropita = new Ropa(); 
+ropita.describirProducto(); 
+ropita.calcularDescuento(); 
+
+const pc = new Electronica(); 
+pc.describirProducto(); 
+pc.calcularDescuento()
